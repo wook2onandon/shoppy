a0f363fbc2cbbd292215af68557d8c2c28af4956
wook2onandon <babyldk@gmail.com>
2023-01-11 19:13:12 +0900

    add custom hook & code refactoring

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -7,9 +7,7 @@ export function AuthContextProvider({ children }) {
   const [user, setUser] = useState();
 
   useEffect(() => {
-    onUserStateChange((user) => {
-      setUser(user);
-    });
+    onUserStateChange((user) => setUser(user));
   }, []);
 
   return (
